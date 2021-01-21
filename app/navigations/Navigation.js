b957c7559103680eeb00dfa43a447868598d0793
Starkin28 <victoreq00@gmail.com>
2021-01-21 20:23:14 +0100

    added icons with react-native-elements

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import {NavigationContainer} from'@react-navigation/native';
-import {createBottomTabNavigator} from'@react-navigation/bottom-tabs';
-
-import ClubsStack from './stacks/ClubsStack';
-import FavoritesStack from './stacks/FavoritesStack';
-import TopClubsStack from './stacks/TopClubsStack';
-import SearchStack from './stacks/SearchStack';
-import AccountStack from './stacks/AccountStack';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
+import ClubsStack from "./stacks/ClubsStack";
+import FavoritesStack from "./stacks/FavoritesStack";
+import TopClubsStack from "./stacks/TopClubsStack";
+import SearchStack from "./stacks/SearchStack";
+import AccountStack from "./stacks/AccountStack";
 
 const Tab = createBottomTabNavigator();
 
-export default function Navigation(){
-    return(
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="clubs" component={ClubsStack} options={{title: "Clubs"}}/>
-                <Tab.Screen name="favorites" component={FavoritesStack} options={{title: "Favorites"}}/>
-                <Tab.Screen name="top-clubs" component={TopClubsStack} options={{title: "Top Clubs"}}/>
-                <Tab.Screen name="search" component={SearchStack} options={{title: "Search"}}/>
-                <Tab.Screen name="account" component={AccountStack} options={{title: "Account"}}/>
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="my-clubs"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "#00a680",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
+        <Tab.Screen
+          name="clubs"
+          component={ClubsStack}
+          options={{ title: "Clubs" }}
+        />
+        <Tab.Screen
+          name="top-clubs"
+          component={TopClubsStack}
+          options={{ title: "Top 5 Clubs" }}
+        />
+        <Tab.Screen
+          name="my-clubs"
+          component={FavoritesStack}
+          options={{ title: "My Clubs" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={SearchStack}
+          options={{ title: "Search" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={AccountStack}
+          options={{ title: "My Account" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function screenOptions(route, color) {
+  let iconName;
+
+  switch (route.name) {
+    case "clubs":
+      iconName = "compass-outline";
+      break;
+    case "my-clubs":
+      iconName = "heart-outline";
+      break;
+    case "top-clubs":
+      iconName = "star-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
+  );
 }
