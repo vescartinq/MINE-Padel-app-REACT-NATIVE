d01785ee7f22ddc0c1456db1aa67e5f0394de63b
Starkin28 <victoreq00@gmail.com>
2021-02-02 12:46:02 +0100

    dinamic title in Club Screen with navigation.setOptions

--- app/screens/Clubs/Club.js
+++ app/screens/Clubs/Club.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function Club(props) {
     const { navigation, route } = props;
-    
+    const { id, name } = route.params;
+
+    navigation.setOptions({ title: name });
+
     return (
         <View>
             <Text>CLUBS SCREEN</Text>
