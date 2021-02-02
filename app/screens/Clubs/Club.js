8847a1e876110fe2dcfa5ff7419cd06487df5ec9
Starkin28 <victoreq00@gmail.com>
2021-02-02 12:31:32 +0100

    implemented navigation from listClub to Club detail screen

---
+++ app/screens/Clubs/Club.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Club(props) {
+    const { navigation, route } = props;
+    
+    return (
+        <View>
+            <Text>CLUBS SCREEN</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
