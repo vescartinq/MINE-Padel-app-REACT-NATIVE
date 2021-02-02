cbdb821593335fc01342e71a56b9ed79e36ec886
Starkin28 <victoreq00@gmail.com>
2021-02-02 10:06:41 +0100

    downloading data-clubs from firebase

--- app/screens/Clubs/Clubs.js
+++ app/screens/Clubs/Clubs.js
@@ -1,16 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Icon } from "react-native-elements";
+import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Clubs({navigation}) {
     const [user, setUser] = useState(null);
+    const [clubs, setClubs] = useState([]);
+    const [totalClubs, setTotalClubs] = useState(0);
+    const [startClubs, setStartClubs] = useState(null);
+    const limitClubs = 10;
+
+    console.log(clubs)
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged((userInfo) => {
           setUser(userInfo);
         });
       }, []);
+
+      useEffect(() => {
+        db.collection("clubs")
+        .get()
+        .then((snap) => {
+          setTotalClubs(snap.size);
+      });
+
+      const resultClubs = [];
+      db.collection("clubs")
+        .orderBy("createAt", "desc")
+        .limit(limitClubs)
+        .get()
+        .then((response) => {
+          setStartClubs(response.docs[response.docs.length - 1]);
+
+          response.forEach((doc) => {
+            const club = doc.data();
+            club.id = doc.id;
+            resultClubs.push(club);
+          });
+          setClubs(resultClubs);
+        })
+    }, [])
+
 
   return (
     <View style={styles.viewBody}>
