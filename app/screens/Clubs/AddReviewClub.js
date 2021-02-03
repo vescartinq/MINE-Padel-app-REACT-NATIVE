073d2b19561aa0d71714346ee302fe2ffdb12d7b
Starkin28 <victoreq00@gmail.com>
2021-02-03 11:00:14 +0100

    navigation between club screen and review screen running

--- app/screens/Clubs/AddReviewClub.js
+++ app/screens/Clubs/AddReviewClub.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function AddReviewClub(props) {
     const { navigation, route } = props;
+    const { idClub } = route.params;
 
     return (
         <View>
