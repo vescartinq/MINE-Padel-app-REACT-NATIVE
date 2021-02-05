a0ac96f89c711417ded4abfb2974b56e8a8abb69
Starkin28 <victoreq00@gmail.com>
2021-02-05 13:03:06 +0100

    topClubs screen implemented, with list using rating

--- app/screens/TopClubs.js
+++ app/screens/TopClubs.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View,Text } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { View } from "react-native";
+import Toast from "react-native-easy-toast";
+import ListTopclubs from "../components/Ranking/ListTopClubs";
 
-export default function TopClubs(){
-    return (
-        <View>
-            <Text>TopClubs...</Text>
-        </View>
-    )
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
+
+export default function TopClubs({ navigation }) {
+  const [clubs, setClubs] = useState([]);
+  const toastRef = useRef();
+
+  useEffect(() => {
+    db.collection("clubs")
+      .orderBy("rating", "desc")
+      .limit(5)
+      .get()
+      .then((response) => {
+        const clubArray = [];
+        response.forEach((doc) => {
+          const data = doc.data();
+          data.id = doc.id;
+          clubArray.push(data);
+        });
+        setClubs(clubArray);
+      });
+  }, []);
+
+  return (
+    <View>
+      <ListTopclubs clubs={clubs} navigation={navigation} />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+    </View>
+  );
 }
